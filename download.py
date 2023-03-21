c3cb42646f42168ad991669076f7fe1d60750a64
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-03-21 13:50:45 +0530

    Intitialize

---
+++ download.py
@@ -0,0 +1,34 @@
+$apiUrl = "https://store.rg-adguard.net/api/GetFiles"
+
+$productUrl = "https://www.microsoft.com/store/productId/9nblggh5r558" # To Do
+#$productUrl = "https://www.microsoft.com/store/productId/9MSPC6MP8FM4" # Whiteboard
+#$productUrl = "https://www.microsoft.com/store/productId/9WZDNCRFJBB1" # Wireless Display Adapter
+
+$downloadFolder = Join-Path $env:TEMP "StoreDownloads"
+if(!(Test-Path $downloadFolder -PathType Container)) {
+    New-Item $downloadFolder -ItemType Directory -Force
+}
+
+$body = @{
+    type = 'url'
+    url  = $productUrl
+    ring = 'RP'
+    lang = 'en-US'
+}
+
+$raw = Invoke-RestMethod -Method Post -Uri $apiUrl -ContentType 'application/x-www-form-urlencoded' -Body $body
+
+$raw | Select-String '<tr style.*<a href=\"(?<url>.*)"\s.*>(?<text>.*)<\/a>' -AllMatches|
+ % { $_.Matches } |
+ % { 
+    $url = $_.Groups[1].Value
+    $text = $_.Groups[2].Value
+    Write-Host $text
+
+    if($text -match "_(x86|x64|neutral).*appx(|bundle)$") {
+        $downloadFile = Join-Path $downloadFolder $text
+        if(!(Test-Path $downloadFile)) {
+            Invoke-WebRequest -Uri $url -OutFile $downloadFile
+        }
+    }
+}
